f19b916a5dcf2c738617b2fde0d0dace2961eb1c
kytnacode <kytnacode@gmail.com>
2023-07-27 12:27:31 -0700

    Add app entry point

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
 // Entry point
+
+import logger from './util/logger.js';
+import createApp from './app.js';
+import {config} from 'dotenv';
+
+config();
+
+logger.verbose('Environment variables loaded');
+
+logger.verbose('Creating app object');
+
+const app = createApp();
+
+logger.verbose('Getting port');
+
+const port = app.get('port');
+
+logger.info('Starting app');
+
+app.listen(port);
+
+logger.info(`App started on port ${port}`);
