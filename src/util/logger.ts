dcac8d38d0b78eaba1c2ec982c9136592af318a6
kytnacode <kytnacode@gmail.com>
2023-07-29 10:57:24 -0700

    Modify logger
    - Remove environment variables check from logger file
    
    - Split logger into two loggers, one for http logs, and other for the others.
    
    - Split logs files into two different, http.log for http and app.log for the others
    
    - Create a logger wrapper that separates logs for the http logger and the general logger.

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,33 +1,41 @@
-import {createLogger, Logger, transports} from 'winston'; // import logging library
-import MissingEnvironmentVariablesError from './errors/missingEnvironmentVariablesError.js';
+import {createLogger, LogCallback, Logger, transports} from 'winston'; // import logging library
 import {defaultFormat} from './logFormats.js';
 
-const requiredEnvVariables = [
-  'NODE_ENV'
-];
+// create general logger (for example, info, debug, warn, error)
+const generalLogger: Logger = createLogger({
+  format: defaultFormat, // set format
+  // set log level
+  // 'debug' shows debug, verbose, http, info, warn and error messages
+  // 'http' shows http, info, warn and error messages
+  level: process.env.NODE_ENV == 'dev' ? 'debug' : 'info',
+  exitOnError: false,
+  // set transports (outputs) for logging
+  transports: [
+    new transports.Console(),
+    new transports.File({filename: 'app.log'}), // save general logs to file
+  ]
+});
 
-const requiredEnvExists = () => requiredEnvVariables.every(e => Object.keys(process.env).includes(e)); // returns true if required env vars exists
+// create logger only for http requests (for example, GET /api/users)
+const httpLogger = createLogger({
+  format: defaultFormat,
+  // set log level
+  level: 'http',
+  exitOnError: false,
+  transports: [
+    new transports.Console(),
+    new transports.File({filename: 'http.log'}) // save https logs to different file
+  ]
+});
 
-// creates configured logger function
-function createCustomLogger() {
-  if (!requiredEnvExists())
-    throw new MissingEnvironmentVariablesError(...requiredEnvVariables.filter(v => !Object.values(process.env).includes(v)));
-
-  return createLogger({
-    format: defaultFormat, // set format
-    // set log level
-    // 'debug' shows debug, verbose, http, info, warn and error messages
-    // 'http' shows http, info, warn and error messages
-    level: process.env.NODE_ENV == 'dev' ? 'debug' : 'http',
-    exitOnError: false,
-    // set transports (outputs) for logging
-    transports: [
-      new transports.Console(),
-      new transports.File({filename: 'app.log'}),
-    ]
-  });
-}
-
-const logger: Logger = createCustomLogger(); // declare singleton loggers
+// create logger wrapper object
+const logger = {
+  info: (message: string, callback?: LogCallback) => generalLogger.info(message, callback),
+  debug: (message: string, callback?: LogCallback) => generalLogger.debug(message, callback),
+  warn: (message: string, callback?: LogCallback) => generalLogger.warn(message, callback),
+  error: (message: string, callback?: LogCallback) => generalLogger.error(message, callback),
+  verbose: (message: string, callback?: LogCallback) => generalLogger.verbose(message, callback),
+  http: (message: string, callback?: LogCallback) => httpLogger.http(message, callback) // send http logs to http logger
+};
 
 export default logger;
