cf3e6a40b045d7121bc71f0312a30b14dd3e1d67
kytnacode <kytnacode@gmail.com>
2023-07-31 09:04:56 -0700

    Add http logging, user and auth related routes, and api documentation with swagger

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,35 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
+import usersRouter from './users/user.routes.js';
+import logger from './util/logger.js';
+import authRoutes from './auth/auth.routes.js';
+import requireAuthMiddleware from './auth/requireAuthMiddleware.js';
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs';
+import yaml from 'yaml';
+
+const file = fs.readFileSync('./dist/swagger.yaml', 'utf8');
+
+const specs = yaml.parse(file);
 
 function createApp() {
   const app = express();
 
   app.set('port', process.env.PORT || 3000);
+
+  app.use(express.json());
+
+  app.use((req, res, next) => {
+    logger.http(`${req.method} ${req.url}`);
+
+    next();
+  });
+
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs, {explorer: true}));
+
+  app.all('/api', (req: Request, res: Response) => res.send('Hello World!'));
+  app.use('/api/auth', authRoutes);
+  app.use('/api/users', requireAuthMiddleware, usersRouter);
+
 
   return app;
 }
